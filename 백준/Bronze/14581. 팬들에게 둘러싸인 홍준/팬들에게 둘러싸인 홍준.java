1bd8deb88996c633fd5d7735fd47f213b1661156
KimTaeWoo <133329997+rlaxodn322@users.noreply.github.com>
2023-09-17 09:03:50 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 236 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String a= sc.next();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+a+"::fan:");
+        System.out.print(":fan::fan::fan:");
+        
+    }
+}
