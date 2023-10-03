614efb92795a5643d7f469b0c7b8ff3aa2c8960b
TaeWoo <133329997+rlaxodn322@users.noreply.github.com>
2023-10-03 09:10:39 +0900

    [Bronze III] Title: 택시 기하학, Time: 216 ms, Memory: 17876 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        double a =sc.nextDouble();
+        System.out.println(a*a*Math.PI);
+        System.out.print(a*a*2);
+    }
+}
