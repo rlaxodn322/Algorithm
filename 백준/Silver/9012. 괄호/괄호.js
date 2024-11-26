560ff7184c0a0c25f552417e056420dd5fb9abe6
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2024-11-27 08:46:20 +0900

    [Silver IV] Title: 괄호, Time: 100 ms, Memory: 9456 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,31 +1,30 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const output = processTestcase(input);
+console.log(output);
+function isValid(a){
+   let stack = [];
+   for(let char of a){
+     if(char === '('){
+       stack.push(char);
+} else if(char ===')' ) {
+    if(stack.length ===0) {
+      return false;
+}else{
+  stack.pop();
+}
+}
+}
+return stack.length === 0;
+}
+function processTestcase(input){
 let count = Number(input.shift());
 let result = [];
-
 for(let i=0; i<count; i++){
-  let stack = [];
-  let bool = true;
+  
   let a = input[i];
-
-  for(let char of a){
-     if(char ==='('){
-       stack.push(char);
-     }else if(char ===')' ){
-         if(stack.length ===0) {
-           bool = false;
-           break;
-         }else{
-         stack.pop();
-
-    }
-
+  let bool = isValid(a);  
+  result.push(bool ? 'YES' : 'NO') ; 
 }
-
+return result.join('\n');
 }
-  if(stack.length !== 0) {
-     bool = false;
-}
-   result.push(bool ? 'YES' : 'NO') ;
-}
-console.log(result.join('\n'));
