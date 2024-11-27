a22b82928dd7698966b9a3e87536f000fc13dc07
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2024-11-27 14:46:08 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String args[])throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        Long a = Long.parseLong(bf.readLine());
+        System.out.println(a*a*a);
+        System.out.println(3);
+    }
+}
