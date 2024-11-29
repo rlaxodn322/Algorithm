fdb7ac8fe9792e397ab876e89f18bc7e35bce79e
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2024-11-29 09:55:29 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 3912 ms, Memory: 62940 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const count = Number(input[0]);
+const num = input.slice(1).map(line => {
+      const [x, y] = line.split(' ').map(Number);
+      return {x,y};
+});
+
+num.sort((a,b) => {
+    if(a.x === b.x){return a.y - b.y};
+    return a.x - b.x;
+})
+
+num.forEach(a => {console.log(a.x+' '+ a.y)});
