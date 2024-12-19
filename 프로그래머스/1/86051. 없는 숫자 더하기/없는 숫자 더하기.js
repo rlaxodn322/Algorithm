a825699804135d01d5d6c421b289f0541b6319a3
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2024-12-19 15:35:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,11 +1,3 @@
 function solution(numbers) {
-      let arr = [0,1,2,3,4,5,6,7,8,9];
-    
-      let sum = arr.reduce((a,b)=> {
-          if(!numbers.includes(b)){
-              return a + b;
-          }
-          return a;
-      },0)
-      return sum;
+      return 45 - numbers.reduce((a,b) => a+b ,0)
     }
