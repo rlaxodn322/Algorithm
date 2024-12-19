166b4c82618d2ffd9f09d0e440e4c9620bb85dcf
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2024-12-19 11:28:14 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -2,13 +2,17 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 const a = parseInt(input, 10);
 
 function ff(a){
-  if(a===0){
-  return 0;
-} else if(a===1){
- return 1;
-}else{
- return ff(a-1) + ff(a-2);
-}
-}
+  let aa = 0, bb=1, cc;
+  if(a===0)return 0;
+  if(a===1)return 1;
 
+  for(let i=2; i<=a; i++){
+    cc = aa+ bb;
+    aa = bb;
+    bb = cc;
+
+}
+return bb;
+
+}
 console.log(ff(a));
