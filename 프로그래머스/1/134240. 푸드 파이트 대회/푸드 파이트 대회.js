5f92c2ce4e76f2af6dcd5978946ab4ce9136e04c
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2024-12-27 10:06:28 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,11 +1,7 @@
 function solution(food) {
     let left = "";
-    let right = "";
     for(let i=1; i<food.length; i++){
-        let count = Math.floor(food[i]/ 2);
-        left += i.toString().repeat(count);
-        right = i.toString().repeat(count) + right;
+        left += i.toString().repeat(Math.floor(food[i]/2));
     }
-    return left + "0" + right;
-    
+    return left + "0" + [...left].reverse().join('');    
 }
