cfcc4ced4b8489d788a42108855778914bf52d34
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2024-12-30 12:15:45 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,12 +1,9 @@
 function solution(n) {
-    const top = 1234567;
-    if(n===1)return 1;
-    if(n===2)return 2;
-    let arr = new Array(n+1).fill(0);
+    let arr = [];
     arr[1] = 1;
     arr[2] = 2;
     for(let i =3; i<=n; i++){
-        arr[i] = (arr[i-1] + arr[i-2]) % top;
+        arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
     }
     return arr[n];
     
