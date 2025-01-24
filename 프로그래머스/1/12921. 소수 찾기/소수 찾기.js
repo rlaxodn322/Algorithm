2b8acc7652cb95700309a1c784cab6da919d41b0
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2025-01-24 11:21:33 +0900

    [level 1] Title: 소수 찾기, Time: 64.70 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,15 +1,11 @@
 function solution(n) {
-    if(n===1)return 0;
-    let arr = new Array(n+1).fill(true);
-    arr[0] = arr[1] = false;
-    
-    for(let i=2; i * i<=n; i++){
-    
-            for(let j= i * i; j<=n; j+=i){
-                arr[j] = false;
-            }
-     
-      
+    if(n===1) return 0;
+    let arr = new Array(n+1).fill(1);
+    arr[0] = arr[1] = 0;
+    for(let i=2; i * i <= n; i++){
+        for(let j= i*i; j<=n; j+=i){
+            arr[j] = 0;
+        }
     }
-    return arr.filter(Boolean).length;
+    return arr.filter(value => value ===1).length;
 }
