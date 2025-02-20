e8f7f39acfcb280d82d3ebaf1d874ce640a5f22e
TaeWoo! <133329997+rlaxodn322@users.noreply.github.com>
2025-02-20 16:49:58 +0900

    [level 1] Title: 소수 만들기, Time: 7.00 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] nums) {
+        int cnt = 0;
+        //세 개의 숫자 +
+        for(int i=0; i<nums.length-2; i++){
+            for(int j=i+1; j<nums.length-1; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)){
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        return cnt;
+    }
+    //소수 확인 함수
+    public boolean isPrime(int num){
+        if(num <= 1) return false;
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num%i==0)return false;
+        }
+        return true;
+    }
+}
